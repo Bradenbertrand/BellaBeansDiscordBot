76f459632c154de21b4f348b622dee94f4d8764a
Braden Bertrand <Bradenbertrand@outlook.com>
2020-11-24 10:48:09 -0500

    Added logs for testing

--- src/bot.js
+++ src/bot.js
@@ -19,8 +19,10 @@ client.on('ready', () => {
 
 // Create an event listener for messages
 client.on('message', message => {
+    console.log("First two characters of message = " + message.content.substring(0, 2));
     if (message.content.substring(0, 2) === "b!")
-        messages.messageHandler(message);
+        console.log("This message is for the bot!")
+    messages.messageHandler(message);
 });
 
 client.login(process.env.BBBOT_TOKEN);
